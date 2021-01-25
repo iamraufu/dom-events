a78009d8f2956fd30b7472f527b7052e343f1c1b
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-25 13:09:35 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ for (let index = 0; index < authors.length; index++) {
     element.style.marginLeft = '10px';
     element.innerHTML = 'Lekhok-' + (index + 1);
 }
+
+const article = document.getElementById('first-article');
+const newParagraph = document.createElement('p');
+newParagraph.innerHTML = "This is created by JS";
+article.appendChild(newParagraph);
+
+const ul = document.getElementById('gift-list');
+const li = document.createElement('li');
+li.innerHTML = 'Brand New Gift Added by JS';
+ul.appendChild(li);
+
+var elem = document.querySelector('#gift-list');
+// elem.style.display = 'none';
+// elem.parentNode.removeChild(elem);
