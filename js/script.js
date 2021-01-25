c393e373acb79d8fbcc1e18e10d174b9914178f8
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-25 10:12:57 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-document.getElementById(first);
-first.style.color = 'green';
-first.style.fontSize = '25px';
-first.style.fontFamily = 'sans-serif'
+var authors = document.getElementsByClassName('author');
+for (let index = 0; index < authors.length; index++) {
+    const element = authors[index];
+    element.style.backgroundColor = 'yellow';
+    element.style.marginLeft = '10px';
+    element.innerHTML = 'Lekhok-' + (index + 1);
+}
